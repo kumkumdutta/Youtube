90f94685603e5336496a8315d1353663e1538b17
Himanshu Sah <09himanshusah@gmail.com>
2024-12-14 19:41:17 +0530

    format

--- internal_service/controller/db.controller.js
+++ internal_service/controller/db.controller.js
@@ -1,129 +1,128 @@
-const find = async (req,res)=>{
+const find = async (req, res) => {
     let collection = req.query.collection;
     let filterdata = req.body;
     try {
-        if(filterdata.project){
+        if (filterdata.project) {
             project = filterdata.project;
             delete filterdata.project;
         }
-        if(filterdata.sort){
+        if (filterdata.sort) {
             sort = filterdata.sort;
             delete filterdata.sort;
         }
-        if(filterdata.limit){
+        if (filterdata.limit) {
             limit = filterdata.limit;
             delete filterdata.limit;
         }
-        if(filterdata.skip){
+        if (filterdata.skip) {
             skip = filterdata.skip;
             delete filterdata.skip;
         }
-        let data = await global.db_service.find({collection,query:filterdata,project,optionals:{skip,limit,sort}});
+        let data = await global.db_service.find({ collection, query: filterdata, project, optionals: { skip, limit, sort } });
         res.send(data);
     } catch (error) {
         res.status(500).send({ status: false, data: "Internal server error" });
     }
 }
 
-
-const findOne = async (req,res)=>{
+const findOne = async (req, res) => {
     let collection = req.query.collection;
     let filterdata = req.body;
     try {
-        let data = await global.db_service.findOne({collection,query:filterdata,project:{...filterdata}});
+        let data = await global.db_service.findOne({ collection, query: filterdata, project: { ...filterdata } });
         res.send(data);
     } catch (error) {
         res.status(500).send({ status: false, data: "Internal server error" });
     }
 }
 
-const insertOne = async (req,res)=>{
+const insertOne = async (req, res) => {
     let collection = req.query.collection;
     let data = req.body;
     try {
-        await global.db_service.insertOne({collection,data});
+        await global.db_service.insertOne({ collection, data });
         res.send({ status: true, data: "Data inserted successfully" });
     } catch (error) {
         res.status(500).send({ status: false, data: "Internal server error" });
     }
 }
 
-const insertMany = async (req,res)=>{
+const insertMany = async (req, res) => {
     let collection = req.query.collection;
-    let data = req.body;    
+    let data = req.body;
     try {
-        await global.db_service.insertMany({collection,data});
+        await global.db_service.insertMany({ collection, data });
         res.send({ status: true, data: "Data inserted successfully" });
-    } catch (error) {    
-        res.status(500).send({ status: false, data: "Internal server error" }); }
+    } catch (error) {
+        res.status(500).send({ status: false, data: "Internal server error" });
     }
+}
 
-
-    const updateOne = async (req,res)=>{
-        let collection = req.query.collection;
-        let query = req.body.query;
-        let data = req.body.data;
-        try {
-            await global.db_service.updateOne({collection,query,data});
-            res.send({ status: true, data: "Data updated successfully" });
-        } catch (error) {
-            res.status(500).send({ status: false, data: "Internal server error" });
-        }
+const updateOne = async (req, res) => {
+    let collection = req.query.collection;
+    let query = req.body.query;
+    let data = req.body.data;
+    try {
+        await global.db_service.updateOne({ collection, query, data });
+        res.send({ status: true, data: "Data updated successfully" });
+    } catch (error) {
+        res.status(500).send({ status: false, data: "Internal server error" });
     }
+}
 
-    const updateMany = async (req,res)=>{
-        let collection = req.query.collection;
-        let query = req.body.query;
-        let data = req.body.data;
-        try {
-            await global.db_service.updateMany({collection,query,data});
-            res.send({ status: true, data: "Data updated successfully" });
-        } catch (error) {
-            res.status(500).send({ status: false, data: "Internal server error" });
-        }
+const updateMany = async (req, res) => {
+    let collection = req.query.collection;
+    let query = req.body.query;
+    let data = req.body.data;
+    try {
+        await global.db_service.updateMany({ collection, query, data });
+        res.send({ status: true, data: "Data updated successfully" });
+    } catch (error) {
+        res.status(500).send({ status: false, data: "Internal server error" });
     }
+}
 
-    const deleteOne = async (req,res)=>{
-        let collection = req.query.collection;
-        let query = req.body.query;
-        try {
-            await global.db_service.deleteOne({collection,query});
-            res.send({ status: true, data: "Data deleted successfully" });
-        } catch (error) {
-            res.status(500).send({ status: false, data: "Internal server error" });
-        }
+const deleteOne = async (req, res) => {
+    let collection = req.query.collection;
+    let query = req.body.query;
+    try {
+        await global.db_service.deleteOne({ collection, query });
+        res.send({ status: true, data: "Data deleted successfully" });
+    } catch (error) {
+        res.status(500).send({ status: false, data: "Internal server error" });
     }
+}
 
-    const deleteMany = async (req,res)=>{
-        let collection = req.query.collection;
-        let query = req.body.query;
-        try {
-            await global.db_service.deleteMany({collection,query});
-            res.send({ status: true, data: "Data deleted successfully" });
-        } catch (error) {
-            res.status(500).send({ status: false, data: "Internal server error" });
-        }
+const deleteMany = async (req, res) => {
+    let collection = req.query.collection;
+    let query = req.body.query;
+    try {
+        await global.db_service.deleteMany({ collection, query });
+        res.send({ status: true, data: "Data deleted successfully" });
+    } catch (error) {
+        res.status(500).send({ status: false, data: "Internal server error" });
     }
+}
 
-    const aggregate = async (req,res)=>{
-        let collection = req.query.collection;
-        let pipeline = req.body;
-        try {
-            let data = await global.db_service.aggregate({collection,pipeline});
-            res.send(data);
-        } catch (error) {
-            res.status(500).send({ status: false, data: "Internal server error" });
-        }
+const aggregate = async (req, res) => {
+    let collection = req.query.collection;
+    let pipeline = req.body;
+    try {
+        let data = await global.db_service.aggregate({ collection, pipeline });
+        res.send(data);
+    } catch (error) {
+        res.status(500).send({ status: false, data: "Internal server error" });
     }
+}
 
-    export {
-        find,
-        findOne,
-        insertOne,
-        insertMany,
-        updateOne,
-        updateMany,
-        deleteOne,
-        deleteMany,
-        aggregate
-    }
+export {
+    find,
+    findOne,
+    insertOne,
+    insertMany,
+    updateOne,
+    updateMany,
+    deleteOne,
+    deleteMany,
+    aggregate
+}
--- internal_service/utils/db.utils.js
+++ internal_service/utils/db.utils.js
@@ -3,14 +3,14 @@ import * as Packages from '../packages/node.packages.js'
 
 class InternalService {
     #db_name
-    constructor(mongourl,db_name) {
-        if(!InternalService.instance) {
+    constructor(mongourl, db_name) {
+        if (!InternalService.instance) {
             this.db_name = db_name;
             this.client = new Packages.MongoClient
-            (mongourl, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            });
+                (mongourl, {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true,
+                });
             this.connection();
             this.get_db_name()
             InternalService.instance = this;
@@ -20,14 +20,14 @@ class InternalService {
 
     async connection() {
         await this.client.connect();
-       
+
     }
 
     get_db_name() {
-       return this.client.db(this.db_name);
+        return this.client.db(this.db_name);
     }
 
-    async find({collection, query,project,optionals}){
+    async find({ collection, query, project, optionals }) {
         try {
             let skip = 0;
             let limit = 100;
@@ -39,87 +39,82 @@ class InternalService {
             if (optionals?.sort) sort = optionals.sort
             if (project) projection = project
 
-            const data = await this.get_db_name().collection(collection).find(query).sort(sort).skip(skip).limit(limit).toArray();
+            const data = await this.get_db_name().collection(collection).find(query).project(projection).sort(sort).skip(skip).limit(limit).toArray();
             return data
 
         } catch (error) {
-          console.log(error)  
+            console.log(error)
         }
     }
 
-    async findOne({collection, query,project}){
-    
+    async findOne({ collection, query, project }) {
+
         try {
-            const data = await this.get_db_name().collection(collection).findOne(query,{...project});
+            const data = await this.get_db_name().collection(collection).findOne(query, { ...project });
             return data
 
         } catch (error) {
-          console.log(error)
-        }}
-        async insertOne({collection,data}){
-            try {
-                await this.get_db_name().collection(collection).insertOne(data);
-            } catch (error) {
-                console.log(error);
-                
-            }
+            console.log(error)
         }
+    }
 
-        async insertMany({collection,data}){
-            try {
-                await this.get_db_name().collection(collection).insertMany(data);
-            } catch (error) {
-                console.log(error);
-                
-            }
+    async insertOne({ collection, data }) {
+        try {
+            await this.get_db_name().collection(collection).insertOne(data);
+        } catch (error) {
+            console.log(error);
         }
+    }
 
-        async updateOne({collection,query,data}){
-            try {
-                await this.get_db_name().collection(collection).updateOne(query,data);
-            } catch (error) {
-                console.log(error);
-                
-            }
+    async insertMany({ collection, data }) {
+        try {
+            await this.get_db_name().collection(collection).insertMany(data);
+        } catch (error) {
+            console.log(error);
         }
+    }
 
-        async updateMany({collection,query,data}){   
-            try {
-                await this.get_db_name().collection(collection).updateMany(query,data);
-            } catch (error) {
-                console.log(error);
-                
-            }
+    async updateOne({ collection, query, data }) {
+        try {
+            await this.get_db_name().collection(collection).updateOne(query, data);
+        } catch (error) {
+            console.log(error);
         }
+    }
 
-        async deleteOne({collection,query}){
-            try {
-                await this.get_db_name().collection(collection).deleteOne(query);
-            } catch (error) {
-                console.log(error);
-                
-            }
+    async updateMany({ collection, query, data }) {
+        try {
+            await this.get_db_name().collection(collection).updateMany(query, data);
+        } catch (error) {
+            console.log(error);
         }
+    }
 
-        async deleteMany({collection,query}){
-            try {
-                await this.get_db_name().collection(collection).deleteMany(query);
-            } catch (error) {
-                console.log(error);
-                
-            }
+    async deleteOne({ collection, query }) {
+        try {
+            await this.get_db_name().collection(collection).deleteOne(query);
+        } catch (error) {
+            console.log(error);
         }
+    }
 
-        async aggregate({collection,pipeline}){
-            try {
-                const data = await this.get_db_name().collection(collection).aggregate(pipeline).toArray();
-                return data
-            } catch (error) {
-                console.log(error);
-                
-            }
+    async deleteMany({ collection, query }) {
+        try {
+            await this.get_db_name().collection(collection).deleteMany(query);
+        } catch (error) {
+            console.log(error);
         }
+    }
+
+    async aggregate({ collection, pipeline }) {
+        try {
+            const data = await this.get_db_name().collection(collection).aggregate(pipeline).toArray();
+            return data
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 
-export {InternalService}
+export { InternalService }
